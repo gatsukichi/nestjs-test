2f8aa0e56526afb65e4544f6786a4519cdc3ef3f
katsukichi <tlstmdrlf109@gmail.com>
2022-04-17 16:18:25 +0900

    서비스 constructor추가
    ts는 좀더 축약할 수 있다.

--- src/boards/boards.controller.js
+++ src/boards/boards.controller.js
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  // constructor(private boardsService:BoardsService){} ts의 접근제한자를 사용해서 아래코드를 위처럼 축약시킬수 있음
+
+  // boardsService; ts의 경우 위에서 한번 선언해야함.
+  constructor(boardsService) {
+    this.boardsService = boardsService;
+  }
+}
